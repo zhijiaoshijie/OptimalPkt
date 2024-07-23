0b461797672b932502f882c1134fea91c5e28ce6
daibiaoxuwu <dujluo@gmail.com>
2024-07-23 22:50:31 +0800

    fit slope

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+import cmath
 import colorsys
+import math
 import os
 import pickle
-import shutil
-import cmath
-import math
-import sys
-from sklearn.cluster import KMeans
+
 import cupy as cp
 import cupyx.scipy.fft as fft
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import chirp
-from scipy import stats
-from tqdm import tqdm
+from sklearn.cluster import KMeans
 
 
 class ModulusComputation:
@@ -33,10 +30,11 @@ class Config:
     symb_cnt = 148
     symb_truth = np.array([
         110, 43, 91, 78, 10, 109, 39, 109, 76, 103, 110, 10, 90, 52, 40, 80, 6, 12, 118, 68, 104, 79, 99, 59, 120, 23,
-        84, 64, 123, 11, 108, 55, 20, 48, 69, 102, 25, 50, 29, 40, 96, 71, 115, 86, 4, 8, 113, 63, 125, 2, 0, 51, 3, 7,
-        15, 31, 99, 57, 27, 61, 104, 47, 31, 31, 68, 121, 108, 22, 96, 62, 67, 70, 113, 21, 80, 89, 125, 5, 116, 9, 42,
-        92, 115, 30, 55, 17, 28, 24, 38, 75, 84, 121, 61, 123, 54, 76, 110, 45, 94, 127, 6, 115, 25, 19, 56, 127, 2, 0,
-        6, 12, 24, 48, 8, 16, 58, 26, 27, 55, 16, 63, 92, 78, 17, 88, 113, 28, 120, 46, 85, 90, 119, 31, 59, 9, 44, 120])
+        84, 64, 123, 11, 108, 55, 20, 48, 69, 102, 25, 50, 29, 40, 96, 71, 115, 86, 4, 8, 113, 63, 125, 2, 1, 52, 4, 8,
+        16, 32, 100, 58, 28, 62, 105, 48, 32, 32, 69, 122, 109, 23, 97, 63, 68, 71, 114, 22, 81, 90, 126, 6, 117, 10,
+        43, 93, 116, 31, 56, 18, 29, 25, 39, 76, 85, 122, 62, 124, 55, 77, 111, 46, 95, 0, 7, 116, 26, 20, 57, 0, 3, 1,
+        7, 13, 25, 49, 9, 17, 59, 27, 28, 56, 17, 64, 93, 79, 18, 89, 114, 29, 121, 47, 86, 91, 120, 32, 60, 10, 45, 121
+    ])
 
     nsamp = round(n_classes * fs / bw)
 
@@ -44,6 +42,7 @@ class Config:
     code_len = 2
     fft_upsamp = 128
     sfdpos = preamble_len + code_len
+    sfdend = sfdpos + 2
     debug = False
 
     t = np.linspace(0, nsamp / fs, nsamp + 1)[:-1]
@@ -101,12 +100,11 @@ def add_freq(pktdata_in, est_cfo_freq):
     return pktdata2a
 
 
-def work(pktdata_in):
+def coarse_work(pktdata_in):
     argmax_est_time_shift_samples = 0
     argmax_est_cfo_samples = 0
     argmax_val = 0
     fft_n = Config.nsamp * Config.fft_upsamp
-
     # integer detection
     for est_time_shift_samples in range(Config.nsamp * 2):
 
@@ -126,106 +124,62 @@ def work(pktdata_in):
             argmax_val = max_val
             argmax_est_time_shift_samples = est_time_shift_samples
             argmax_est_cfo_samples = cp.argmax(fft_raw)
-
     if argmax_est_cfo_samples > fft_n / 2:
         argmax_est_cfo_samples -= fft_n
     est_cfo_freq = argmax_est_cfo_samples.get() * (Config.fs / fft_n)
     est_to_s = argmax_est_time_shift_samples / Config.fs
-
     # print(f'{argmax_est_time_shift_samples=}, {argmax_est_cfo_samples=}, {fft_n=}, {est_cfo_freq=} Hz, {est_to_s=} s)')
     pktdata2a = add_freq(pktdata_in, - est_cfo_freq)
     pktdata2a = np.roll(pktdata2a, -argmax_est_time_shift_samples)  # !!! TODO est_to_dec
+    return est_cfo_freq, pktdata2a
+
+
+def work(pkt_totcnt, pktdata_in):
+    fft_n = Config.nsamp * Config.fft_upsamp
+    est_cfo_freq, pktdata2a = coarse_work(pktdata_in)
     # second detection
     # ====
-    symb_cnt = Config.sfdpos + 5  # len(pktdata)//Config.nsamp
-    ndatas = pktdata2a[: symb_cnt * Config.nsamp].reshape(symb_cnt, Config.nsamp)
-
-    upsamp = Config.fft_upsamp
-    ans1, power1 = dechirp(ndatas, Config.downchirp, upsamp)
-    ans2, power2 = dechirp(ndatas, Config.upchirp, upsamp)
-    vals = cp.zeros((symb_cnt,), dtype=cp.float64)
-    for i in range(symb_cnt - (Config.sfdpos + 2)):
-        power = cp.sum(power1[i: i + Config.preamble_len]) + cp.sum(
-            power2[i + Config.sfdpos: i + Config.sfdpos + 2])
-        ans = cp.abs(cp.sum(cp.exp(1j * 2 * cp.pi / Config.n_classes * ans1[i: i + Config.preamble_len])))
-        vals[i] = power * ans
-    detect = cp.argmax(vals)
-
-    ansval = ModulusComputation.average_modulus(ans1[detect: detect + Config.preamble_len], Config.n_classes)
-
-    sfd_upcode = ansval.get()
-    ansval2 = ModulusComputation.average_modulus(ans2[detect + Config.sfdpos: detect + Config.sfdpos + 2],
-                                                 Config.n_classes)
-
-    sfd_downcode = ansval2.get()
-    re_cfo_0 = ModulusComputation.average_modulus((sfd_upcode, sfd_downcode), Config.n_classes)
-    est_to_0 = ModulusComputation.average_modulus((sfd_upcode, - sfd_downcode), Config.n_classes)
-    print(' '.join([f'{x:.3f}' for x in ans1[: Config.preamble_len]]),
-          ' '.join([f'{x:.3f}' for x in ans2[Config.sfdpos: Config.sfdpos + 2]]), sfd_upcode, sfd_downcode, re_cfo_0,
-          est_to_0, detect)
-
-    est_to_0 = est_to_0.get().item()
-    re_cfo_0 = re_cfo_0.get().item()
-    re_cfo_freq = re_cfo_0 * (Config.fs / fft_n)
-    est_to_int = round(est_to_0)
-    est_to_dec = est_to_0 - est_to_int
-    pktdata3 = add_freq(pktdata2a, - re_cfo_freq)
-    pktdata3 = np.roll(pktdata3, -est_to_int)
-
+    est_to_dec, est_to_int, pktdata3, re_cfo_0, re_cfo_freq = fine_work(pktdata2a)
+    # print(f"{re_cfo_0=}, {est_to_int=}, {est_to_dec=}")
     all_cfo_freq = re_cfo_freq + est_cfo_freq
-    all_sfo_freq = all_cfo_freq / Config.sig_freq * Config.bw
 
-    symb_cnt = 15
-    ndatas2a = pktdata3[: symb_cnt * Config.nsamp].reshape(symb_cnt, Config.nsamp)
+    detect, upsamp = test_preamble(est_to_dec, pktdata3)
 
-    print(f"{re_cfo_0=}, {est_to_int=}, {est_to_dec=}")
-    upsamp = Config.fft_upsamp
-    detect = 0
-    ans1, power1 = dechirp(ndatas2a[detect: detect + Config.preamble_len], Config.downchirp, upsamp)
-    ans1 = ans1.get()
-    ans1 += est_to_dec / 8
-    ans2, power2 = dechirp(ndatas2a[detect + Config.sfdpos: detect + Config.sfdpos + 2], Config.upchirp, upsamp)
-    ans2 = ans2.get()
-    ans2 += est_to_dec / 8
-    print('preamble: ' + " ".join([f'{x:.3f}' for x in ans1]) + 'sfd: ' + " ".join([f'{x:.3f}' for x in ans2]),
-          est_to_dec / 8)
+    pktdata4 = pktdata3[Config.nsamp * (detect + Config.sfdpos + 2) + Config.nsamp // 4:]
 
-    pktdata2 = pktdata3[Config.nsamp * (detect + Config.sfdpos + 2) + Config.nsamp // 4:]
-
-    if len(pktdata2) < Config.symb_cnt * Config.nsamp:
-        print(f'short signal: {len(pktdata2)=} {len(pktdata2) / Config.nsamp=} < {Config.nsamp=}')
+    if len(pktdata4) < Config.symb_cnt * Config.nsamp:
+        print(f'{pkt_totcnt=} short signal: {len(pktdata4)=} {len(pktdata4) / Config.nsamp=} < {Config.symb_cnt=}')
         return []
-    ndatas = pktdata2[: Config.symb_cnt * Config.nsamp].reshape(Config.symb_cnt, Config.nsamp)
-    ans1, power1 = dechirp(ndatas[detect + Config.sfdpos + 2:], Config.downchirp)
-    ans1n = ans1.get()
-    ans1n += est_to_dec
-    ans1r = [round(x) for x in ans1n]
-    if not min(power1) > np.mean(power1) / 2:
-        print(f'power1 drops: {" ".join([str(round(x.item())) for x in power1])}')
-        return []
-    if (ans1r != Config.symb_truth).any():
-        for idx in range(len(ans1r)):
-            if ans1r[idx] != Config.symb_truth[idx]: print(f'{idx=} {ans1n[idx]=:.3f} {Config.symb_truth[idx]=}', end=' ')
-        print()
+    # ans1n, ndatas = decode_payload(detect, est_to_dec, pktdata4)
 
+    # print('decoded data', ' '.join([f'{x:.3f}' for x in ans1n]))
+    # sfo correction
+    est_cfo_slope = all_cfo_freq / Config.sig_freq * Config.bw * Config.bw / Config.n_classes
 
+    sig_time = len(pktdata3) / Config.fs
+    # print(all_cfo_freq, 'Hz', est_cfo_slope, 'Hz/s', sig_time)
+    t = np.linspace(0, sig_time, len(pktdata3) + 1)[:-1]
+    chirpI1 = chirp(t, f0=0, f1=- est_cfo_slope * sig_time, t1=sig_time, method='linear', phi=90)
+    chirpQ1 = chirp(t, f0=0, f1=- est_cfo_slope * sig_time, t1=sig_time, method='linear', phi=0)
+    est_cfo_symbol = cp.array(chirpI1 + 1j * chirpQ1)
+    pktdata5 = pktdata3 * est_cfo_symbol
+    detect, upsamp = test_preamble(est_to_dec, pktdata5)
 
-    ans1r = [x - int(x) for x in ans1n]
-    ans1u = np.unwrap(ans1r, period=1)
-    slope, intercept, r, p, std_err = stats.linregress(list(range(len(ans1r))), ans1u)
-    ans1new = ans1n - np.array([(slope * i + intercept) - int(slope * i + intercept) for i in range(len(ans1r))])
-    if Config.debug: print(f'fit: {slope} samples')
+    est_to_dec2, est_to_int2, pktdata6, re_cfo_2, re_cfo_freq_2 = fine_work(pktdata5)
+    all_cfo_freq = re_cfo_freq + est_cfo_freq + re_cfo_freq_2
+    detect, upsamp = test_preamble(est_to_dec2, pktdata6)
+    pktdata7 = pktdata6[Config.nsamp * (detect + Config.sfdpos + 2) + Config.nsamp // 4:]
+    ans2n, ndatas = decode_payload(detect, est_to_dec, pktdata7, pkt_totcnt)
 
-    #     est_sfo_t = - Config.n_classes * (1 - Config.freq_sig / (Config.freq_sig + re_cfo)) # ???????????????????
-    freq_sig_est = all_cfo_freq / (1 / (1 - slope / Config.n_classes) - 1)
-    print('estimated frequency:', freq_sig_est/1e6, 'mhz')
+    # print('decoded data', ' '.join([f'{x:.0f}' for x in ans2n]))
 
-
+    # ans1new = ans1n - np.array([est_cfo_slope * (i + Config.sfdpos + 2.5) for i in range(len(ans1r))])
+    # print('fixed data  ', " ".join([f'{x:.3f}' for x in ans1new]))
 
     '''
     if Config.debug: print('est_to_dec / 8', est_to_dec / (Config.nsamp / Config.n_classes))
     ans1n += est_to_dec / (Config.nsamp / Config.n_classes)  # ???????????????'''  # TODO !!!
-    print('decoded data', ' '.join([f'{x:.3f}' for x in ans1n]))
+
     angles = []
     for dataY in ndatas[detect + Config.sfdpos + 4:]:
         opts = Config
@@ -246,6 +200,73 @@ def work(pktdata_in):
                 diff_avg += math.pi * 2
             angles.append(diff_avg)
     return angles
+
+
+def decode_payload(detect, est_to_dec, pktdata4, pkt_totcnt):
+    ndatas = pktdata4[: Config.symb_cnt * Config.nsamp].reshape(Config.symb_cnt, Config.nsamp)
+    ans1, power1 = dechirp(ndatas[detect + Config.sfdpos + 2:], Config.downchirp)
+    ans1n = ans1.get()
+    ans1n += est_to_dec / 8
+    ans1r = [round(x) % Config.n_classes for x in ans1n]
+    if not min(power1) > np.mean(power1) / 2:
+        print(f'power1 drops: {" ".join([str(round(x.item())) for x in power1])}')
+    if (ans1r != Config.symb_truth).any():
+        print(f'{pkt_totcnt=}', end=' ')
+        for idx in range(len(ans1r)):
+            if ans1r[idx] != Config.symb_truth[idx]: print(f'{idx=} {ans1n[idx]=:.3f} {Config.symb_truth[idx]=}',
+                                                           end=' ')
+        print()
+    return ans1n, ndatas
+
+
+def test_preamble(est_to_dec, pktdata3):
+    ndatas2a = pktdata3[: Config.sfdend * Config.nsamp].reshape(Config.sfdend, Config.nsamp)
+
+    upsamp = Config.fft_upsamp
+    detect = 0
+    ans1, power1 = dechirp(ndatas2a[detect: detect + Config.preamble_len], Config.downchirp, upsamp)
+    ans1 = ans1.get()
+    ans1 += est_to_dec / 8
+    ans2, power2 = dechirp(ndatas2a[detect + Config.sfdpos: detect + Config.sfdpos + 2], Config.upchirp, upsamp)
+    ans2 = ans2.get()
+    ans2 += est_to_dec / 8
+    # print('preamble: ' + " ".join([f'{x:.3f}' for x in ans1]) + 'sfd: ' + " ".join([f'{x:.3f}' for x in ans2]), est_to_dec / 8)
+    # print(power1)
+    return detect, upsamp
+
+
+def fine_work(pktdata2a):
+    fft_n = Config.nsamp * Config.fft_upsamp
+    symb_cnt = Config.sfdpos + 5  # len(pktdata)//Config.nsamp
+    ndatas = pktdata2a[: symb_cnt * Config.nsamp].reshape(symb_cnt, Config.nsamp)
+    upsamp = Config.fft_upsamp
+    ans1, power1 = dechirp(ndatas, Config.downchirp, upsamp)
+    ans2, power2 = dechirp(ndatas, Config.upchirp, upsamp)
+    vals = cp.zeros((symb_cnt,), dtype=cp.float64)
+    for i in range(symb_cnt - (Config.sfdpos + 2)):
+        power = cp.sum(power1[i: i + Config.preamble_len]) + cp.sum(
+            power2[i + Config.sfdpos: i + Config.sfdpos + 2])
+        ans = cp.abs(cp.sum(cp.exp(1j * 2 * cp.pi / Config.n_classes * ans1[i: i + Config.preamble_len])))
+        vals[i] = power * ans
+    detect = cp.argmax(vals)
+    ansval = ModulusComputation.average_modulus(ans1[detect: detect + Config.preamble_len], Config.n_classes)
+    sfd_upcode = ansval.get()
+    ansval2 = ModulusComputation.average_modulus(ans2[detect + Config.sfdpos: detect + Config.sfdpos + 2],
+                                                 Config.n_classes)
+    sfd_downcode = ansval2.get()
+    re_cfo_0 = ModulusComputation.average_modulus((sfd_upcode, sfd_downcode), Config.n_classes)
+    est_to_0 = ModulusComputation.average_modulus((sfd_upcode, - sfd_downcode), Config.n_classes)
+    # print(' '.join([f'{x:.3f}' for x in ans1[: Config.preamble_len]]),
+    #      ' '.join([f'{x:.3f}' for x in ans2[Config.sfdpos: Config.sfdpos + 2]]), sfd_upcode, sfd_downcode, re_cfo_0,
+    #      est_to_0, detect)
+    est_to_0 = est_to_0.get().item()
+    re_cfo_0 = re_cfo_0.get().item()
+    re_cfo_freq = re_cfo_0 * (Config.fs / fft_n)
+    est_to_int = round(est_to_0)
+    est_to_dec = est_to_0 - est_to_int
+    pktdata3 = add_freq(pktdata2a, - re_cfo_freq)
+    pktdata3 = np.roll(pktdata3, -est_to_int)
+    return est_to_dec, est_to_int, pktdata3, re_cfo_0, re_cfo_freq
 
 
 # read packets from file
@@ -280,8 +301,8 @@ if __name__ == "__main__":
         kmeans = KMeans(n_clusters=2, random_state=0)
         kmeans.fit(nmaxs.reshape(-1, 1))
         thresh = np.mean(kmeans.cluster_centers_)
-        #counts, bins = np.histogram(nmaxs, bins=100)
-        #print(counts, bins, kmeans.cluster_centers_, thresh)
+        # counts, bins = np.histogram(nmaxs, bins=100)
+        # print(counts, bins, kmeans.cluster_centers_, thresh)
 
         pkt_totcnt = 0
 
@@ -301,7 +322,7 @@ if __name__ == "__main__":
                 if nmax < thresh:
                     if len(pktdata) > 14 and pkt_cnt > 20:
                         if Config.debug: print(f"start parsing pkt {pkt_totcnt} len: {len(pktdata)}")
-                        angles.extend(work(cp.concatenate(pktdata)))
+                        angles.extend(work(pkt_totcnt, cp.concatenate(pktdata)))
                         color = [colorsys.hls_to_rgb((hue + 0.5) / 3, 0.4, 1) for hue in [0, 1, 2]]
 
                         name = 'angles'
