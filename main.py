4c6b330cbb05279a6b2eac6d604735db63d6b097
daibiaoxuwu <dujluo@gmail.com>
2024-10-15 21:30:24 +0800

    temp

--- main.py
+++ main.py
@@ -49,37 +49,36 @@ def test():
     data[t_all < istart / Config.fs] = 0
     data[t_all >= (istart + Config.nsamp) / Config.fs] = 0
 
-    dataX = data
-
     phase3 = 0
     t = cp.arange(Config.nsamp, dtype=cp.float32) * 1 / Config.fs
-    t0 = 0
-    # t = cp.linspace(0, (Config.nsamp + 1) / Config.fs, Config.nsamp + 1)[:-1]
-    f0 = Config.bw / 2
-    f1 = -Config.bw / 2
-    beta = (f1 - f0) / (2 ** Config.sf / Config.bw)
-    downchirp = 2 * cp.pi * (f0 * (t - t0) + 0.5 * beta * (t - t0) ** 2) + phase3
+    fig = go.Figure()
+    for t0 in (0, t[1] * 10):
+        # t = cp.linspace(0, (Config.nsamp + 1) / Config.fs, Config.nsamp + 1)[:-1]
+        f0 = Config.bw / 2
+        f1 = -Config.bw / 2
+        beta = (f1 - f0) / (2 ** Config.sf / Config.bw)
+        downchirp = 2 * cp.pi * (f0 * (t - t0) + 0.5 * beta * (t - t0) ** 2) + phase3
 
-    dataX += downchirp
+        dataX = data + downchirp
 
-    code = symb_idx
-    kk = int(Config.fs / Config.bw)
-    dataX1 = dataX[:-code * kk]
-    dataX2 = dataX[-code * kk:]
+        code = symb_idx
+        kk = int(Config.fs / Config.bw)
+        dataX1 = dataX[:-code * kk]
+        dataX2 = dataX[-code * kk:]
 
-    freq = code * Config.bw / Config.n_classes
-    fsig = t * 2 * cp.pi * freq
-    fsig2 = t * 2 * cp.pi * (freq - Config.bw)
+        freq = code * Config.bw / Config.n_classes
+        fsig = t * 2 * cp.pi * freq
+        fsig2 = t * 2 * cp.pi * (freq - Config.bw)
 
-    fig = px.line(dataX.get())
-    fig.add_trace(go.Scatter(y=fsig.get(), mode='lines', name='fit', line=dict(color='red')))
-    fig.add_trace(go.Scatter(y=fsig2.get(), mode='lines', name='fit2', line=dict(color='red')))
+        fig.add_trace(go.Scatter(y=dataX.get(), mode='lines', name='fit', line=dict(color='blue')))
+        fig.add_trace(go.Scatter(y=fsig.get(), mode='lines', name='fit', line=dict(color='red')))
+        fig.add_trace(go.Scatter(y=fsig2.get(), mode='lines', name='fit2', line=dict(color='red')))
+        d1 = dataX1 - fsig[:-code * kk]
+        d2 = dataX2 - fsig2[-code * kk:]
+        print(f"d1avg: {cp.mean(d1)} d2avg: {cp.mean(d2)}")
     fig.update_layout(title="dataX, fsig, fsig2")
     fig.show()
 
-    d1 = dataX1 - fsig[:-code * kk]
-    d2 = dataX2 - fsig2[-code * kk:]
-    print(f"d1avg: {cp.mean(d1)} d2avg: {cp.mean(d2)}")
 
 
 if __name__ == "__main__":
