093dfa2c9405bb482400faef14151d8afd374b2f
daibiaoxuwu <dujluo@gmail.com>
2024-12-23 16:53:34 +0800

    1d

--- objective.py
+++ objective.py
@@ -73,61 +73,6 @@ def objective_decode(est_cfo_f, est_to_s, pktdata_in):
     # for pidx in range(Config.sfdpos + 2, round(Config.total_len)):
     dvx = []
     for pidx in range(32, Config.preamble_len):
-        # fig = FigureResampler(go.Figure(layout_title_text=f"OL fit {pidx=} {est_cfo_f=:.3f} {est_to_s=:.3f}"))
-        if False:# for deltaf in np.arange(0, 200, 10):
-            deltaf = 0
-            estf = est_cfo_f + deltaf
-            start_pos_all_new = nsamp_small * pidx * (1 - estf / Config.sig_freq) + est_to_s
-            start_pos = round(start_pos_all_new)
-            xv = cp.arange(start_pos - 100, start_pos + 100)
-            fig = px.scatter(x=xv, y=tocpu(cp.unwrap(cp.angle(pktdata_in[xv]))), title=f"{pidx=} head")
-            fig.add_vline(x=start_pos_all_new)
-            x_data = np.arange(start_pos + 100, start_pos + Config.nsamp - 100)
-            y_data = np.unwrap(np.angle(tocpu(pktdata_in[x_data])))
-            coefficients = np.polyfit(x_data, y_data, 2)
-            print(coefficients)
-
-            y_val2 = cp.exp(1j * togpu(np.polyval(coefficients, x_data)))
-            pow = pktdata_in[x_data].dot(y_val2)
-            print(cp.abs(pow), cp.sum(cp.abs(pktdata_in[x_data])))
-
-            fig = go.Figure(layout_title_text = f"{pidx=} head")
-            fig.add_trace(go.Scatter(x=x_data, y=y_data-np.polyval(coefficients, x_data), mode='lines'))
-            # fig.add_trace(go.Scatter(x=x_data, y=np.polyval(coefficients, x_data), mode='lines'))
-            fig.show()
-            fig = px.line(x=x_data, y=tocpu(cp.abs(cp.cumsum(pktdata_in[x_data] * y_val2))))
-
-            beta = Config.bw / ((2 ** Config.sf) / Config.bw) / Config.fs / Config.fs * np.pi
-            betanew = beta * (1 + 2 * estf / Config.sig_freq)
-            print(betanew)
-            fig.show()
-
-            y_data_1d = y_data - np.polyval((betanew, 0, 0), x_data)
-            coefficients_1d = np.polyfit(x_data, y_data_1d, 1)
-            print(coefficients_1d)
-            coefficients[0] = betanew
-            coefficients[1:] = coefficients_1d
-
-            fig = go.Figure(layout_title_text=f"{pidx=} head")
-            fig.add_trace(go.Scatter(x=x_data, y=y_data - np.polyval(coefficients, x_data), mode='lines'))
-            fig.show()
-            y_val2 = cp.exp(1j * togpu(np.polyval(coefficients, x_data)))
-            fig = px.line(x=x_data, y=tocpu(cp.abs(cp.cumsum(pktdata_in[x_data] * y_val2))))
-            fig.show()
-
-            continue
-        if True:
-            deltaf = 0
-            estf = est_cfo_f + deltaf
-            start_pos_all_new = nsamp_small * pidx * (1 - estf / Config.sig_freq) + est_to_s
-            start_pos = round(start_pos_all_new)
-            x_data = np.arange(start_pos + 100, start_pos + Config.nsamp - 100)
-            y_data = np.unwrap(np.angle(tocpu(pktdata_in[x_data])))
-            coefficients = np.polyfit(x_data, y_data, 2)
-            dvx.append(coefficients[0])
-            continue
-
-        if False:
             estf = est_cfo_f
             start_pos_all_new = nsamp_small * pidx * (1 - estf / Config.sig_freq) + est_to_s
             start_pos = round(start_pos_all_new)
@@ -138,8 +83,7 @@ def objective_decode(est_cfo_f, est_to_s, pktdata_in):
             y_data_1d = y_data - np.polyval((betanew, 0, 0), x_data)
             coefficients_1d = np.polyfit(x_data, y_data_1d, 1)
             print(coefficients_1d)
-            coefficients[0] = betanew
-            coefficients[1:] = coefficients_1d
+            coefficients = (betanew, *coefficients_1d)
 
             fig = go.Figure(layout_title_text=f"{pidx=} head")
             fig.add_trace(go.Scatter(x=x_data, y=y_data - np.polyval(coefficients, x_data), mode='lines'))
@@ -147,41 +91,10 @@ def objective_decode(est_cfo_f, est_to_s, pktdata_in):
             y_val2 = cp.exp(1j * togpu(np.polyval(coefficients, x_data)))
             fig = px.line(x=x_data, y=tocpu(cp.abs(cp.cumsum(pktdata_in[x_data] * y_val2))))
             fig.show()
-            continue
 
-
-            tstandard = cp.linspace(0, Config.nsamp / Config.fs, Config.nsamp + 1)[:-1]
-            refchirp = mychirp(tstandard, f0=Config.bw * -0.5 , f1=Config.bw * 0.5, t1=2 ** Config.sf / Config.bw)
-            dt = (start_pos - start_pos_all_new) / Config.fs
-            beta = Config.bw / ((2 ** Config.sf) / Config.bw)
-            df = -estf #+ dt * beta)
-            sig1 = pktdata_in[start_pos: Config.nsamp + start_pos]
-
-
-            x = (cp.arange(len(pktdata_in)) - est_to_s) * (1 + estf / Config.sig_freq)
-            yi = cp.zeros_like(x, dtype=np.complex64)
-            bwnew = Config.bw * (1 + estf / Config.sig_freq)
-            betanew = beta * (1 + 2 * estf / Config.sig_freq)
-            for i in range(Config.preamble_len):
-                mask = (i * Config.nsampf < x) & (x <= (i + 1) * Config.nsampf)
-                yi[mask] = cp.exp(2j * cp.pi * (betanew / 2 * (x[mask] - i * Config.nsampf) ** 2 / Config.fs**2 + (- bwnew / 2 + estf) * (x[mask] - i * Config.nsampf)/Config.fs))
-
-            diffdata = np.diff(tocpu(cp.unwrap(cp.angle(pktdata_in[xv]*cp.conj(yi[xv])))), prepend=0)
-            d2data = np.ones_like(diffdata)
-            for i in range(1, Config.preamble_len):
-                d2data[abs(x[xv] - i * Config.nsampf) < Config.gen_refchirp_deadzone] = 0
-            p2 = pktdata_in[xv] * togpu(d2data)
-            # val = cp.abs(cp.conj(togpu(yi[xv])).dot(p2))
-            # val2 = cp.concatenate( [cp.abs(cp.cumsum(cp.conj(togpu(yi[xv][round(i * Config.nsampf): round((i+1)*Config.nsampf)])) * p2[round(i * Config.nsampf): round((i+1)*Config.nsampf)])) for i in range(0, Config.preamble_len)])
-            val3 = cp.array( [cp.angle(cp.conj(togpu(yi[xv][round(i * Config.nsampf): round((i+1)*Config.nsampf)])) .dot( p2[round(i * Config.nsampf): round((i+1)*Config.nsampf)])) for i in range(0, Config.preamble_len)])
-
-            # fig.add_trace(go.Scatter(x=x[xv], y=tocpu(cp.unwrap(cp.angle(yi[xv])))))
-            # fig.add_trace(go.Scatter(x=x[xv], y=tocpu(cp.unwrap(cp.angle(pktdata_in[xv])))))
-            # fig.add_trace(go.Scatter(x=x[xv], y=tocpu(val2), name=f"{estf=:.3f}"))
-        # fig.show()
     fig = px.line(dvx)
     beta = Config.bw / ((2 ** Config.sf) / Config.bw) / Config.fs / Config.fs * np.pi
-    betanew = beta * (1 + 2 * estf / Config.sig_freq)
+    betanew = beta * (1 + 2 * est_cfo_f / Config.sig_freq)
     fig.add_hline(betanew)
     fig.show()
     print((np.mean(dvx) / beta - 1) * Config.sig_freq / 2)
