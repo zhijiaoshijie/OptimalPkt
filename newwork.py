e9d3a57e4e048a00d863038dc6cca822278544ea
daibiaoxuwu <dujluo@gmail.com>
2025-01-13 17:55:40 +0800

    work

--- newwork.py
+++ newwork.py
@@ -86,26 +86,77 @@ def plot_fit2d(coefficients_2d_in, estf, estt, pidx, pktdata_in):
     tinytsymbrp = cp.linspace(tstart, tend, int(1e6))
     pltfig1(tinytsymbrp,
             wrap(cp.polyval(coefficients_2d, tinytsymbrp)),
-            fig = fig).show()
+            fig = fig, line=dict(dash='dash')).show()
 
 
-def pltfig(xdata, ydatas, title = None, yaxisrange = None, modes = None, marker = None, addvline = None, addhline = None, fig = None):
+def plot_fit2d_after_refine(coefficients_2d_in, coef2d_refined_in, estf, estt, pidx, pktdata_in):
+    logger.warning(f'''
+    plt_fit2d after_refine called with {coefficients_2d_in=} {coef2d_refined_in=} {estf=} {pidx=}
+    draw 3 imgs:
+        1. the original 2d unwrapped, with fitted 2d poly curve
+        2. the difference between them, unwrapped
+        3. the original 2d nounwrapped, with fitted 2d poly curve wrapped in 
+    ''')
+    coefficients_2d = coefficients_2d_in.copy()
+    coef2d_refined = coef2d_refined_in.copy()
+    nsymblen = 2 ** Config.sf / Config.bw * Config.fs * (1 - estf / Config.sig_freq)
+    tsymblen = nsymblen / Config.fs
+    nstart = pidx * nsymblen + estt * Config.fs
+    tstart = nstart / Config.fs
+    tend = tstart + tsymblen
+    nsymbr = cp.arange(round(nstart) + 1000, round(nstart) + Config.nsamp - 1000)
+    tsymbr = nsymbr / Config.fs
+    p0idx = 1000
+    nsymbrp = cp.arange(round(nstart) - p0idx, round(nstart) + Config.nsamp + 1000)
+    assert nsymbrp[p0idx] == round(nstart)
+    tsymbrp = nsymbrp / Config.fs
+
+    uarp = cp.unwrap(cp.angle(pktdata_in[nsymbrp]))
+    coefficients_2d[-1] -= (cp.polyval(coefficients_2d, tsymbrp[p0idx]) - uarp[p0idx])
+    coefficients_2d[-1] += cp.angle(pktdata_in[nsymbr].dot(cp.exp(-1j * cp.polyval(coefficients_2d, tsymbr))))
+    coef2d_refined[-1] -= (cp.polyval(coef2d_refined, tsymbrp[p0idx]) - uarp[p0idx])
+    coef2d_refined[-1] += cp.angle(pktdata_in[nsymbr].dot(cp.exp(-1j * cp.polyval(coef2d_refined, tsymbr))))
+    logger.warning(f"inside plt_fit2d: {uarp[p0idx] - cp.polyval(coefficients_2d, tsymbrp[p0idx])}")
+    pltfig(tsymbrp,
+           (uarp, cp.polyval(coefficients_2d, tsymbrp), cp.polyval(coef2d_refined, tsymbrp)),
+           title = f"{pidx=} fit 2d uwa curve after_refine",
+           addvline = (tstart, tend)).show()
+    pltfig(tsymbrp,
+           (uarp - cp.polyval(coefficients_2d, tsymbrp), uarp - cp.polyval(coef2d_refined, tsymbrp)),
+            title=f"{pidx=} fit diff between 2d curve and uwa after_refine",
+            modes=('lines+markers','lines+markers'),
+            marker=dict(size=1),
+            yaxisrange=[-2, 2],
+            addvline = (tstart, tend)).show()
+    fig = pltfig1(tsymbrp,
+                  cp.angle(pktdata_in[nsymbrp]),
+                  mode='markers',
+                  title=f"{pidx=} fit 2d no-uw angles after_refine",
+                  addvline=(tstart, tend))
+    tinytsymbrp = cp.linspace(tstart, tend, int(1e6))
+    fig = pltfig(tinytsymbrp,
+                 (wrap(cp.polyval(coefficients_2d, tinytsymbrp)), wrap(cp.polyval(coef2d_refined, tinytsymbrp))),
+            fig = fig)
+    fig.show()
+
+def pltfig(xdata, ydatas, title = None, yaxisrange = None, modes = None, marker = None, addvline = None, addhline = None, fig = None, line=None):
     if fig is None: fig = go.Figure(layout_title_text=title)
     elif title is not None: fig.update_layout(title_text=title)
     if modes is None:
         modes = ['lines' for _ in ydatas]
-    for ydata, mode in zip(ydatas, modes):
-        fig.add_trace(go.Scatter(x=tocpu(xdata), y=tocpu(ydata), mode=mode, marker=marker))
+    for idx, (ydata, mode) in enumerate(zip(ydatas, modes)):
+        if line == None and idx == 1: line = dict(dash='dash')
+        fig.add_trace(go.Scatter(x=tocpu(xdata), y=tocpu(ydata), mode=mode, marker=marker, line=line))
     pltfig_hind(addhline, addvline, fig, yaxisrange)
     return fig
 
 
 
-def pltfig1(xdata, ydata, title = None, yaxisrange = None, mode = None, marker = None, addvline = None, addhline = None, fig = None):
+def pltfig1(xdata, ydata, title = None, yaxisrange = None, mode = None, marker = None, addvline = None, addhline = None, fig = None, line=None):
     if fig is None: fig = go.Figure(layout_title_text=title)
     elif title is not None: fig.update_layout(title_text=title)
     if mode is None: mode = 'lines'
-    fig.add_trace(go.Scatter(x=tocpu(xdata), y=tocpu(ydata), mode=mode, marker=marker))
+    fig.add_trace(go.Scatter(x=tocpu(xdata), y=tocpu(ydata), mode=mode, marker=marker, line=line))
     pltfig_hind(addhline, addvline, fig, yaxisrange)
     return fig
 
@@ -117,7 +168,7 @@ def pltfig_hind(addhline, addvline, fig, yaxisrange):
         for y in addvline: fig.add_hline(y=y, line_dash='dash')
 
 
-def fitcoef(estf, estt, pktdata_in):
+def fitcoef(estf, estt, pktdata_in, fitmethod = "2dfit", searchquad = False):
     nestt = estt * Config.fs
     betai = Config.bw / ((2 ** Config.sf) / Config.bw) * np.pi
     betat = betai * (1 + estf / Config.sig_freq)
@@ -127,15 +178,14 @@ def fitcoef(estf, estt, pktdata_in):
     coeflist = []
     for pidx in range(0, Config.preamble_len):
         nstart = pidx * nsymblen + nestt
-        tstart = nstart / Config.fs
+        nsymbr = cp.arange(round(nstart) + 1000, round(nstart + nsymblen) - 1000)
+        tsymbr = nsymbr / Config.fs
         # nstart = np.polyval(coeff_time, pidx) * Config.fs
 
         #
         # 2d fit on unwrapped angle
         #
-        nsymbr = cp.arange(round(nstart) + 1000, round(nstart + nsymblen) - 1000)
-        tsymbr = nsymbr / Config.fs
-        fitmethod = "2dfit"
+
         if fitmethod == "2dfit":
             coefficients_2d = cp.polyfit(tsymbr, cp.unwrap(cp.angle(pktdata_in[nsymbr])), 2)
         elif fitmethod == "1dfit":
@@ -159,29 +209,47 @@ def fitcoef(estf, estt, pktdata_in):
         #
         # find accurate coefficients_2d by power
         #
-        val = obj(tsymbr, pktdata_in[nsymbr], coefficients_2d)
-        logger.warning(
-            f'find accurate coefficients_2d by power. before start, result from linear fit {coefficients_2d=} {val=}')
+        coef2d_refined = refine_coef(estf, estt, pidx, pktdata_in, coefficients_2d, searchquad = searchquad)
+        coeflist.append(coef2d_refined)
 
-        rangeval = 0.005
-        for i in range(20):
-            for j in range(2):
-                xvals = cp.linspace(coefficients_2d[j] * (1 - rangeval), coefficients_2d[j] * (1 + rangeval), 1001)
-                yvals = []
-                for x in xvals:
-                    coef2 = coefficients_2d.copy()
-                    coef2[j] = x
-                    yvals.append(obj(tsymbr, y_data, coef2))
-                oldv = coefficients_2d[j]
-                coefficients_2d[j] = xvals[cp.argmax(yvals)]
-                valnew = obj(tsymbr, y_data, coefficients_2d)
-                if True:  # valnew < val*(1-1e-7):
-                    fig = go.Figure(layout_title_text=f"{pidx=} {i=} {j=} {val=} {valnew=}")
-                    fig.add_trace(go.Scatter(x=xvals, y=yvals))
-                    fig.add_vline(x=oldv)
-                    fig.show()
-                assert valnew >= val * (1 - 1e-7), f"{val=} {valnew=} {i=} {j=} {coefficients_2d=} {val-valnew=}"
-                if abs(valnew - val) < 1e-7: rangeval /= 2
-                val = valnew
-        coefficients_2d[-1] -= cp.angle(y_data.dot(cp.exp(-1j * cp.polyval(coefficients_2d, tsymbr))))
-        coeflist.append(coefficients_2d)
+        plot_fit2d_after_refine(coefficients_2d, coef2d_refined, estf, estt, pidx, pktdata_in)
+        sys.exit(0)
+    return cp.array(coeflist)
+
+def refine_coef(estf, estt, pidx, pktdata_in, coefficients_2d_in, searchquad = False):
+    coefficients_2d = coefficients_2d_in.copy()
+    nsymblen = 2 ** Config.sf / Config.bw * Config.fs * (1 - estf / Config.sig_freq)
+    nstart = pidx * nsymblen + estt * Config.fs
+    nsymbr = cp.arange(round(nstart) + 1000, round(nstart + nsymblen) - 1000)
+    tsymbr = nsymbr / Config.fs
+
+    val = obj(tsymbr, pktdata_in[nsymbr], coefficients_2d)
+    logger.warning(
+        f'find accurate coefficients_2d by power. before start, result from linear fit {coefficients_2d=} {val=}')
+
+    rangeval = 0.005
+    for i in range(20):
+
+        # search for both 1st (quad) and 2nd (linear) value, or only 2nd value
+        for j in range(0 if searchquad else 1, 2):
+            xvals = cp.linspace(coefficients_2d[j] * (1 - rangeval), coefficients_2d[j] * (1 + rangeval), 1001)
+            yvals = []
+            for x in xvals:
+                coef2 = coefficients_2d.copy()
+                coef2[j] = x
+                yvals.append(obj(tsymbr, pktdata_in[nsymbr], coef2))
+            oldv = coefficients_2d[j]
+            coefficients_2d[j] = xvals[cp.argmax(yvals)]
+            valnew = obj(tsymbr, pktdata_in[nsymbr], coefficients_2d)
+            if valnew < val*(1-1e-7):
+                fig = go.Figure(layout_title_text=f"{pidx=} {i=} {j=} {val=} {valnew=}")
+                fig.add_trace(go.Scatter(x=xvals, y=yvals))
+                fig.add_vline(x=oldv)
+                fig.show()
+            assert valnew >= val * (1 - 1e-7), f"{val=} {valnew=} {i=} {j=} {coefficients_2d=} {val-valnew=}"
+            if abs(valnew - val) < 1e-7: rangeval /= 2
+            val = valnew
+
+    # align with phase. here may have a 2pi difference when evaluated in unwrap
+    coefficients_2d[-1] -= cp.angle(pktdata_in[nsymbr].dot(cp.exp(-1j * cp.polyval(coefficients_2d, tsymbr))))
+    return coefficients_2d
