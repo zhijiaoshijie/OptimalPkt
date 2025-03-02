f5a00296d5f50471a69fea6fe4c5938ac1663114
daibiaoxuwu <dujluo@gmail.com>
2025-03-03 00:42:23 +0800

    relabel

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 import time
 import csv
@@ -13,20 +14,23 @@ from mainwork import mainwork
 if __name__ == "__main__":
     if not os.path.exists(Config.outfolder): os.makedirs(Config.outfolder)
 
-    script_path = __file__
-    mod_time = os.path.getmtime(script_path)
-    readable_time = time.ctime(mod_time)
-    logger.warning(f"Last modified time of the script: {readable_time}")
+    beta = Config.bw / ((2 ** Config.sf) / Config.bw)
+    tstandard = cp.arange(Config.nsamp) / Config.fs
+    refchirp = cp.exp(-1j * 2 * cp.pi * (-Config.bw * 0.5 * tstandard + 0.5 * beta * tstandard * tstandard))
 
-    fulldata = []
-    # Main loop read files
-    pkt_idx_cnt = 0
-    logger.warning("ERR this only work for integer")
-
-    dpath = "/data/djl/datasets/msudata_ljk/"
-    outpath = f"/data/djl/datasets/msudata_ljk_cut/{Config.name}/cover_sf{Config.sf}"
+    dpath = "/data/djl/datasets/msudata_ljk_cut_old/farm/cover_sf10"
+    opath = "/data/djl/datasets/msudata_ljk_cut/farm/cover_sf10"
     for fname in os.listdir(dpath):
-        if f"{Config.name}-cover-{Config.sf}-" in fname:
-            file_path = os.path.join(dpath, fname)
-            opath = os.path.join(outpath, fname.split("-")[-1])
-            preprocess_file(file_path, opath)
+        for fname2 in os.listdir(os.path.join(dpath, fname)):
+            for fname3 in os.listdir(os.path.join(dpath, fname, fname2)):
+                rawdata = cp.fromfile(os.path.join(dpath, fname, fname2, fname3))
+                data1 = cp.matmul(Config.decode_matrix_a, rawdata)
+                data2 = cp.matmul(Config.decode_matrix_b, rawdata)
+                vals = cp.abs(data1) ** 2 + cp.abs(data2) ** 2
+                coderet = cp.argmax(vals).item()
+                pidx, _, pktidx, sf = fname3.split("_")
+                outpath = os.path.join(opath, fname, fname2, fname3)
+                if not os.path.exists(outpath): os.makedirs(outpath)
+                rawdata.tofile(os.path.join(outpath, f"{pidx}_{str(coderet)}_{pktidx}_{sf}"))
+
+
--- utils.py
+++ utils.py
@@ -130,7 +130,7 @@ class Config:
 
     betai = bw / ((2 ** sf) / bw)
     for code in range(n_classes):
-        if (code-1)%4!=0 and code>=11: continue
+        # if (code-1)%4!=0 and code>=11: continue
         nsamples = around(nsamp / n_classes * (n_classes - code))
         f01 = bw * (-0.5 + code / n_classes)
         refchirpc1 = cp.exp(-1j * 2 * cp.pi * (f01 * tstandard + 0.5 * betai * tstandard * tstandard))
