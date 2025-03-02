4bfd653c2b72f44f000f0e90850a7def19ebda1f
daibiaoxuwu <dujluo@gmail.com>
2025-03-03 01:18:34 +0800

    relabel another folder

--- main.py
+++ main.py
@@ -18,19 +18,20 @@ if __name__ == "__main__":
     tstandard = cp.arange(Config.nsamp) / Config.fs
     refchirp = cp.exp(-1j * 2 * cp.pi * (-Config.bw * 0.5 * tstandard + 0.5 * beta * tstandard * tstandard))
 
-    dpath = "/data/djl/datasets/msudata_ljk_cut_old/farm/cover_sf10"
-    opath = "/data/djl/datasets/msudata_ljk_cut/farm/cover_sf10"
-    for fname in os.listdir(dpath):
-        for fname2 in os.listdir(os.path.join(dpath, fname)):
-            for fname3 in os.listdir(os.path.join(dpath, fname, fname2)):
-                rawdata = cp.fromfile(os.path.join(dpath, fname, fname2, fname3))
-                data1 = cp.matmul(Config.decode_matrix_a, rawdata)
-                data2 = cp.matmul(Config.decode_matrix_b, rawdata)
-                vals = cp.abs(data1) ** 2 + cp.abs(data2) ** 2
-                coderet = cp.argmax(vals).item()
-                pidx, _, pktidx, sf = fname3.split("_")
-                outpath = os.path.join(opath, fname, fname2, fname3)
-                if not os.path.exists(outpath): os.makedirs(outpath)
-                rawdata.tofile(os.path.join(outpath, f"{pidx}_{str(coderet)}_{pktidx}_{sf}"))
+    for Config.name in ['farm', 'lot']:
+        dpath = f"/data/djl/datasets/msudata_ljk_cut_old/{Config.name}/clean_data/sf{Config.sf}"
+        opath = f"/data/djl/datasets/msudata_ljk_cut/{Config.name}/clean_data/sf{Config.sf}"
+        for fname in os.listdir(dpath):
+                for fname3 in os.listdir(os.path.join(dpath, fname)):
+                    rawdata = cp.fromfile(os.path.join(dpath, fname, fname3))
+                    data1 = cp.matmul(Config.decode_matrix_a, rawdata)
+                    data2 = cp.matmul(Config.decode_matrix_b, rawdata)
+                    vals = cp.abs(data1) ** 2 + cp.abs(data2) ** 2
+                    coderet = cp.argmax(vals).item()
+                    pidx, _, pktidx, sf = fname3.split("_")
+                    outpath = os.path.join(opath, fname)
+                    if not os.path.exists(outpath): os.makedirs(outpath)
+                    rawdata.tofile(os.path.join(outpath, f"{pidx}_{str(coderet)}_{pktidx}_{sf}"))
+        os.system(f"tar -czf {Config.name}_clean_sf{Config.sf}.tar.gz -C /data/djl/datasets/msudata_ljk_cut/{Config.name}/clean_data sf{Config.sf}")
 
 
