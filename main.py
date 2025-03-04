2e3ff55222746ad3df3dbee512a82d06649e6b69
daibiaoxuwu <dujluo@gmail.com>
2025-03-04 11:17:11 +0800

    debug read complex

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ from work import *
 from reader import *
 from mainwork import mainwork
 
-def decode_loratrimmer(rawdata):
+def decode_loratrimmer(rawdata, name):
     data1 = cp.matmul(Config.decode_matrix_a, rawdata)
     data2 = cp.matmul(Config.decode_matrix_b, rawdata)
     vals = cp.abs(data1) ** 2 + cp.abs(data2) ** 2
     coderet = cp.argmax(vals).item()
-    amp = cp.sum(cp.abs(rawdata)) ** 2
-    logger.warning(cp.sum(cp.abs(rawdata)))
-    pltfig1(None, vals, title=os.path.join('farm/cover_sf10/groundtruth', fname, fname2), addvline=(int(fname2.split('_')[1]),), addhline=(amp,)).show()
+    amp = cp.zeros(Config.n_classes, dtype=float)
+    for i in range(Config.n_classes):
+        tsplit = around(Config.nsampf / Config.n_classes * (Config.n_classes - i))
+        amp[i] = cp.sum(cp.abs(rawdata[:tsplit])) ** 2 + cp.sum(cp.abs(rawdata[tsplit:])) ** 2
+    logger.warning(cp.mean(cp.abs(rawdata)))
+    fig = pltfig1(None, vals, title=os.path.join(f'farm/cover_sf10/{name}', fname, fname2) + ' : ' + str(coderet))#, addvline=(int(fname2.split('_')[1]),))
+    pltfig1(None, amp, fig = fig).show()
+    pltfig1(None, rawdata.imag).show()
 
 # read packets from file
 if __name__ == "__main__":
@@ -26,11 +31,14 @@ if __name__ == "__main__":
     beta = Config.bw / ((2 ** Config.sf) / Config.bw)
     tstandard = cp.arange(Config.nsamp) / Config.fs
     refchirp = cp.exp(-1j * 2 * cp.pi * (-Config.bw * 0.5 * tstandard + 0.5 * beta * tstandard * tstandard))
-    dpath = "/data/djl/datasets/msudata_ljk_cut/farm/cover_sf10/groundtruth"
+    # name = "1mile"
+    name = "groundtruth"
+    dpath = f"/data/djl/datasets/msudata_ljk_cut/farm/cover_sf10/{name}"
     for fname in os.listdir(dpath):
-        for fname2 in os.listdir(os.path.join(dpath, fname)):
-            rawdata = cp.fromfile(os.path.join(dpath, fname, fname2))
-            decode_loratrimmer(rawdata)
+        for fname2 in os.listdir(os.path.join(dpath, fname))[2:]:
+            print(fname2.split("_")[1])
+            rawdata = cp.fromfile(os.path.join(dpath, fname, fname2), dtype=cp.complex64)
+            decode_loratrimmer(rawdata, name)
             sys.exit(0)
 
 
