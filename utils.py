b7974095ec3b7c15e7026f474d12c48ee43fb83e
daibiaoxuwu <dujluo@gmail.com>
2025-03-05 13:13:58 +0800

    utils detect_range_pkts 12 -> 20

--- utils.py
+++ utils.py
@@ -169,7 +169,7 @@ class Config:
     if not os.path.exists(figpath): os.mkdir(figpath)
 
     fft_upsamp = 1024
-    detect_range_pkts = 12
+    detect_range_pkts = 20
     assert detect_range_pkts >= 2 # add 1, for buffer of cross-add
     detect_to_max = nsamp * 2
     fft_n = int(fs) #nsamp * fft_upsamp
